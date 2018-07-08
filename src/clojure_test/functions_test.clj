63aa3a608e4ab311a2fc154c23539400a81211bd
Arthur Quinn <arthurquinn92@gmail.com>
2018-07-08 12:42:11 -0400

    continue function test from clojure documentation

--- src/clojure_test/functions_test.clj
+++ src/clojure_test/functions_test.clj
@@ -11,9 +11,20 @@
   ([x] (str "Hello, " x "!"))
   ([x y] (str x ", " y "!")))
 
-; Throw a logical exception if any of these expressions evaluate to false
+; throw a compiler exception if any of these expressions evaluate to false
 (assert (= "Hello, World!" (greeting)))
 (assert (= "Hello, Clojure!" (greeting "Clojure")))
 (assert (= "Good morning, Clojure!" (greeting "Good morning" "Clojure")))
 
+(defn do-nothing [x] x)
 
+(defn always-thing [& x] :thing)
+
+(defn make-thingy [x] (fn [& args] x))
+
+; test out make-thingy
+(let [n (rand-int Integer/MAX_VALUE)
+      f (make-thingy n)]
+  (assert (= n (f)))
+  (assert (= n (f :foo)))
+  (assert (= n (apply f :foo (range)))))
